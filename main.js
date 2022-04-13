e513b78da3bdf5ed60b9b3fe549e6318978ceb35
nikhileshraju <83688797+nikhileshraju@users.noreply.github.com>
2022-04-13 14:55:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(640, 480);
+canvas.center();
+}
+
+function draw(){
+
+}
+
+function save_filtered_image(){
+save("Mustache_filter_image");
+}
